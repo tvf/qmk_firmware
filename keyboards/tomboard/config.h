99ef338b9517e82c9253719bbb3aeffe4fe7756b
Thomas Foster <thomasvfoster@gmail.com>
2018-10-14 12:08:25 +0100

    buildable firmware for full board

--- keyboards/tomboard/config.h
+++ keyboards/tomboard/config.h
@@ -41,8 +41,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
 */
-#define MATRIX_ROW_PINS { D0, D5 } // TODO what's the pin numbering scheme?
-#define MATRIX_COL_PINS { F1, F0, B0 }
+#define MATRIX_ROW_PINS { D0, D1, D2, D3 } // TODO what's the pin numbering scheme?
+#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, B6, B5, B4, E6, D7, C6, D4 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL, or CUSTOM_MATRIX */
--- keyboards/tomboard/keymaps/default/keymap.c
+++ keyboards/tomboard/keymaps/default/keymap.c
@@ -15,84 +15,68 @@
  */
 #include QMK_KEYBOARD_H
 
-// Defines the keycodes used by our macros in process_record_user
-enum custom_keycodes {
-  QMKBEST = SAFE_RANGE,
-  QMKURL
-};
+enum tomboard_layers { qwerty, numeric, function };
 
-struct tomboard_layers {
-    enum type {
-        qwerty,
-        lower,
-        raise,
-        thingy
-    };
-};
+#define MONUM MO(numeric)
+#define MOFUN MO(function)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Qwerty
  * ,---------------------------. .---------------------------.
- * |Tab| Q | W | E | R | T |   | |   | Y | U | I | O | P |Bks|
+ * |Tab| Q | W | E | R | T |[{ | |-_ | Y | U | I | O | P |Bks|
  * |---+---+---+---+---+---+---+ +---+---+---+---+---+---+---|
- * |Esc| A | A | S | D | F |   | |   | H | J | K | L | ; | " |
+ * |Esc| A | S | D | F | G |]} | |=+ | H | J | K | L |;: |'" |
  * |---+---+---+---+---+---+---+ +---+---+---+---+---+---+---|
- * |Shf| Z | X | C | V | B |   | |   | N | M | , | . | / |Ent|
+ * |LSh| Z | X | C | V | B |`~ | |\| | N | M |,< |.> |?/ |RSh|
  * |---+---+---+---+---+---+---+ +---+---+---+---+---+---+---|
- * |   |Crl|Alt|Hyp|Lo |Spc|   | |   |Spc|Hi |   |   |   |   |
+ * |Ctl|Alt|Sup|Num|Fun|Spc|Ent| |Ent|Spc|Fun|Num|   |RAl|Ctl|
  * `---------------------------' `---------------------------'
  */
+ [qwerty] = {
+     { KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LBRC, KC_MINS, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC },
+     { KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_RBRC, KC_EQL,  KC_H,  KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT },
+     { KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_GRAVE, KC_BSLS, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT },
+     { KC_LCTL, KC_LALT, KC_LGUI, MONUM, MOFUN, KC_SPC, KC_ENT, KC_ENT, KC_SPC, MOFUN, MONUM, KC_NO, KC_RALT, KC_RCTL }
+ },
 
-/* Lo
+/* Num
  * ,---------------------------. .---------------------------.
- * |Tab| 1 | 2 | 3 | 4 | 5 |   | |   | 6 | 7 | 8 | 9 | 0 |Bks|
+ * |   | 1 | 2 | 3 | 4 | 5 |   | |   | 6 | 7 | 8 | 9 | 0 |Del|
  * |---+---+---+---+---+---+---+ +---+---+---+---+---+---+---|
- * |Esc| A | A | S | D | F |   | |   | H | J | K | L | ; | " |
+ * |   | ! | @ | # | $ | % |   | |   | ^ | & | * | ( | ) |   |
  * |---+---+---+---+---+---+---+ +---+---+---+---+---+---+---|
- * |Shf| Z | X | C | V | B |   | |   | N | M | , | . | / |Ent|
+ * |   |   |   |   |   |   |   | |   |<- | v | ^ | ->|   |   |
  * |---+---+---+---+---+---+---+ +---+---+---+---+---+---+---|
- * |   |Crl|Alt|Hyp|Lo |Spc|   | |   |Spc|Hi |   |   |   |   |
+ * |   |   |   |   |   |   |   | |   |   |   |   |   |   |   |
  * `---------------------------' `---------------------------'
  */
+ [numeric] = {
+     { KC_NO, KC_1, KC_2, KC_3, KC_4, KC_5, KC_NO, KC_NO, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL },
+     { KC_NO, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_NO, KC_NO, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_NO },
+     { KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_DOWN,    KC_UP,    KC_RIGHT, KC_RPRN, KC_TRNS },
+     { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
+ },
 
-/* Hi
+/* Fun
  * ,---------------------------. .---------------------------.
- * |Tab| Q | W | E | R | T |   | |   | Y | U | I | O | P |Bks|
+ * |   |F1 |F2 |F3 |F4 |F5 |F6 | |   |   |   | 1 | 2 | 3 |   |
  * |---+---+---+---+---+---+---+ +---+---+---+---+---+---+---|
- * |Esc| A | A | S | D | F |   | |   | H | J | K | L | ; | " |
+ * |   |F7 |F8 |F9 |F10|F11|F12| |   |   |   | 4 | 5 | 6 |   |
  * |---+---+---+---+---+---+---+ +---+---+---+---+---+---+---|
- * |Shf| Z | X | C | V | B |   | |   | N | M | , | . | / |Ent|
+ * |   |   |   |   |   |   |   | |   |   |   | 7 | 8 | 9 |   |
  * |---+---+---+---+---+---+---+ +---+---+---+---+---+---+---|
- * |   |Crl|Alt|Hyp|Lo |Spc|   | |   |Spc|Hi |   |   |   |   |
+ * |   |   |   |   |   |   |   | |   |   |   |   | 0 |   |   |
  * `---------------------------' `---------------------------'
  */
-    [tomboard_layers::qwerty] = {
-        KC_TAB, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
-        KC_TAB, KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
-        KC_TAB, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT ,
-        KC_TAB, BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
-    },
+ [function] = {
+     { KC_NO,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NO, KC_NO, KC_NO, KC_1, KC_2, KC_3, KC_NO },
+     { KC_NO,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NO, KC_NO, KC_NO, KC_4, KC_5, KC_6, KC_NO },
+     { KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_7, KC_8, KC_9, KC_TRNS },
+     { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
+ },
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case QMKBEST:
-      if (record->event.pressed) {
-        // when keycode QMKBEST is pressed
-        SEND_STRING("QMK is the best thing ever!");
-      } else {
-        // when keycode QMKBEST is released
-      }
-      break;
-    case QMKURL:
-      if (record->event.pressed) {
-        // when keycode QMKURL is pressed
-        SEND_STRING("https://qmk.fm/" SS_TAP(X_ENTER));
-      } else {
-        // when keycode QMKURL is released
-      }
-      break;
-  }
   return true;
 }
 
